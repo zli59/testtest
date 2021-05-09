544ddf75b5efb80e443593bbeeb3f33ace20bd37
Zhuoming Li <zli59@syr.edu>
2021-05-09 00:06:53 -0400

    First commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+// CopyRight [2021]
+// CopyOwner: Zhuoming Li
+
+#include "./xib2lbpi.h"
+#include "addVar.cpp"
+#include "addVar_sim.cpp"
+#include "StripDataFile.cpp"
+#include "TMVA_Lb.cpp"
+#include "TMVA_Xib.cpp"
+
+void main() {
+  std::cout << "LOVE QQ!" << std::endl;
+}
